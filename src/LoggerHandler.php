a84359118e380d1b58a1f1bf0afe9f5383f0c536
Sabar Tampubolon <sabartampubolon@gmail.com>
2019-02-20 10:54:53 +0700

    cleaned up code

--- src/LoggerHandler.php
+++ src/LoggerHandler.php
@@ -31,45 +31,6 @@ class LoggerHandler extends AbstractProcessingHandler
         $this->name  = $name;
     }
 
-    // /**
-    //  * @param array $record
-    //  *
-    //  * @return LoggerMessage
-    //  */
-    // protected function getMessage(array $record)
-    // {
-    //     $loggerColour = new LoggerColour($record['level_name']);
-
-    //     if ($this->style == 'card') {
-    //         // Include context as facts to send to microsoft teams
-    //         // Added Sent Date Info
-    //         $facts = array_merge($record['context'], [[
-    //             'name'  => 'Sent Date',
-    //             'value' => date('D, M d Y H:i:s e'),
-    //         ]]);
-
-    //         return new LoggerMessage([
-    //             'summary'    => $record['level_name'] . ($this->name ? ': ' . $this->name : ''),
-    //             'themeColor' => (string) $loggerColour,
-    //             'sections'   => [
-    //                 [
-    //                     'activityTitle'    => $this->name,
-    //                     'activitySubtitle' => '<span style="color:#' . (string) $loggerColour . '">' . $record['level_name'] . '</span>',
-    //                     'activityText'     => $record['message'],
-    //                     'activityImage'    => (string) new LoggerAvatar($record['level_name'], $loggerColour),
-    //                     'facts'            => $facts,
-    //                     'markdown'         => true
-    //                 ]
-    //             ]
-    //         ]);
-    //     } else {
-    //         return new LoggerMessage([
-    //             'text'       => ($this->name ? $this->name . ' - ' : '') . '<span style="color:#' . (string) $loggerColour . '">' . $record['level_name'] . '</span>: ' . $record['message'],
-    //             'themeColor' => (string) $loggerColour,
-    //         ]);
-    //     }
-    // }
-
     /**
      * @param array $record
      *
